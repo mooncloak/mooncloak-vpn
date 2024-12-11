094e572d6072a13a17984a208fe8f2b8c6e9756a
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-12-11 16:33:40 -0500

    Added global unexpected error string resource

--- app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/dependency/DependencyLicenseListViewModel.kt
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/dependency/DependencyLicenseListViewModel.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Stable
 import com.mikepenz.aboutlibraries.Libs
 import com.mooncloak.kodetools.konstruct.annotations.Inject
 import com.mooncloak.kodetools.statex.ViewModel
+import com.mooncloak.vpn.app.shared.resource.Res
+import com.mooncloak.vpn.app.shared.resource.global_unexpected_error
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.getString
 
 @Stable
 public class DependencyLicenseListViewModel @Inject public constructor(
@@ -11,26 +15,28 @@ public class DependencyLicenseListViewModel @Inject public constructor(
 ) : ViewModel<DependencyLicenseListStateModel>(initialStateValue = DependencyLicenseListStateModel()) {
 
     public fun load() {
-        emit(value = state.current.value.copy(isLoading = true))
+        coroutineScope.launch {
+            emit(value = state.current.value.copy(isLoading = true))
 
-        try {
-            val libs = Libs.Builder()
-                //.withJson(block()) TODO: Load the Libraries File
-                .build()
+            try {
+                val libs = Libs.Builder()
+                    //.withJson(block()) TODO: Load the Libraries File
+                    .build()
 
-            emit(
-                value = state.current.value.copy(
-                    isLoading = false,
-                    libs = libs
+                emit(
+                    value = state.current.value.copy(
+                        isLoading = false,
+                        libs = libs
+                    )
                 )
-            )
-        } catch (e: Exception) {
-            emit(
-                value = state.current.value.copy(
-                    isLoading = false,
-                    errorMessage = e.message
+            } catch (e: Exception) {
+                emit(
+                    value = state.current.value.copy(
+                        isLoading = false,
+                        errorMessage = e.message ?: getString(Res.string.global_unexpected_error)
+                    )
                 )
-            )
+            }
         }
     }
 }
