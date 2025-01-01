55a75f933538fec4e4a30c764270c54e82dd7b24
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-01-01 16:11:12 -0500

    Implemented AndroidLocalNetworkManager

--- app-shared/src/androidMain/kotlin/com.mooncloak.vpn.app.shared/api/network/AndroidLocalNetworkManager.kt
+++ app-shared/src/androidMain/kotlin/com.mooncloak.vpn.app.shared/api/network/AndroidLocalNetworkManager.kt
@@ -1,12 +1,70 @@
 package com.mooncloak.vpn.app.shared.api.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.InetAddresses
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import androidx.core.content.getSystemService
 import com.mooncloak.kodetools.konstruct.annotations.Inject
 import com.mooncloak.vpn.app.shared.util.ApplicationContext
+import java.net.InetAddress
 
 public class AndroidLocalNetworkManager @Inject public constructor(
     private val context: ApplicationContext
 ) : LocalNetworkManager {
 
     // TODO: Implement AndroidLocalNetworkManager
-    override suspend fun getInfo(): LocalNetworkInfo? = null
+    override suspend fun getInfo(): LocalNetworkInfo = LocalNetworkInfo(
+        ipAddress = getDeviceIpAddress(context)
+    )
+
+    private fun getDeviceIpAddress(context: Context): String? {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return null
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return null
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> getWifiIpAddress(context)
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> getCellularIpAddress(
+                connectivityManager
+            )
+
+            else -> null
+        }
+    }
+
+    private fun getWifiIpAddress(context: Context): String? {
+        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiInfo = wifiManager.connectionInfo
+        val ipAddress = wifiInfo.ipAddress
+
+        return intToIp(ipAddress)
+    }
+
+    private fun getCellularIpAddress(connectivityManager: ConnectivityManager): String? {
+        val networks = connectivityManager.allNetworks
+        for (network in networks) {
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
+            if (capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true) {
+                val linkProperties = connectivityManager.getLinkProperties(network)
+                for (address in linkProperties?.linkAddresses.orEmpty()) {
+                    val inetAddress = address.address
+                    if (!inetAddress.isLoopbackAddress && inetAddress is InetAddress) {
+                        return inetAddress.hostAddress
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+    private fun intToIp(ipAddress: Int): String? = InetAddress.getByAddress(
+        byteArrayOf(
+            (ipAddress ushr 24).toByte(),
+            (ipAddress ushr 16).toByte(),
+            (ipAddress ushr 8).toByte(),
+            ipAddress.toByte()
+        )
+    ).hostAddress
 }
