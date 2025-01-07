c5cd22aa55fee55644b427036d09760e1d0400f6
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-01-07 15:30:04 -0500

    Updated ServerConnectedLayout for ServerConnectionScreen

--- app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/server/connection/composable/ServerConnectedLayout.kt
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/server/connection/composable/ServerConnectedLayout.kt
@@ -1,9 +1,22 @@
 package com.mooncloak.vpn.app.shared.feature.server.connection.composable
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.mooncloak.vpn.app.shared.api.server.Server
+import com.mooncloak.vpn.app.shared.resource.Res
+import com.mooncloak.vpn.app.shared.resource.server_connection_connected_description
+import com.mooncloak.vpn.app.shared.resource.server_connection_connected_title
+import com.mooncloak.vpn.app.shared.theme.SecondaryAlpha
 import kotlinx.datetime.Instant
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 internal fun ServerConnectedLayout(
@@ -14,5 +27,27 @@ internal fun ServerConnectedLayout(
     rxThroughput: Long? = null,
     txThroughput: Long? = null
 ) {
+    Column(
+        modifier = modifier.padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(Res.string.server_connection_connected_title),
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
 
+        Text(
+            modifier = Modifier.padding(top = 16.dp, bottom = 32.dp)
+                .fillMaxWidth(),
+            text = stringResource(Res.string.server_connection_connected_description),
+            style = MaterialTheme.typography.bodyMedium.copy(
+                color = MaterialTheme.colorScheme.onSurface.copy(
+                    alpha = SecondaryAlpha
+                )
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
 }
