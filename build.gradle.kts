2f3aba628cbb2191ebbe52d4e3ef8e2ec6fd32d0
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-02 12:13:29 -0500

    Updated Kotlin to version 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.1.0" apply false
-    kotlin("multiplatform") version "2.1.0" apply false
-    kotlin("android") version "2.1.0" apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false
+    kotlin("jvm") version "2.1.10" apply false
+    kotlin("multiplatform") version "2.1.10" apply false
+    kotlin("android") version "2.1.10" apply false
+    kotlin("plugin.serialization") version "2.1.10" apply false
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
     id("com.google.devtools.ksp") apply false
     id("io.ktor.plugin") apply false
