afc48b286d4696cd24b76f64d08b993a23bfc187
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-04 15:47:24 -0500

    Created ConnectToDefaultServerUseCase

---
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/api/server/usecase/ConnectToDefaultServerUseCase.kt
@@ -0,0 +1,20 @@
+package com.mooncloak.vpn.app.shared.api.server.usecase
+
+import com.mooncloak.kodetools.konstruct.annotations.Inject
+import com.mooncloak.vpn.api.shared.vpn.VPNConnectionManager
+import com.mooncloak.vpn.app.shared.di.PresentationScoped
+
+@PresentationScoped
+public class ConnectToDefaultServerUseCase @Inject public constructor(
+    private val getDefaultServer: GetDefaultServerUseCase,
+    private val vpnConnectionManager: VPNConnectionManager
+) {
+
+    public suspend operator fun invoke(): Boolean {
+        val server = getDefaultServer() ?: return false
+
+        vpnConnectionManager.connect(server)
+
+        return true
+    }
+}
