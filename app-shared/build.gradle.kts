5c95dbfa5e00896e605f0bcb178890ddba278233
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-14 17:01:13 -0400

    Added generation of iOS target frameworks

--- app-shared/build.gradle.kts
+++ app-shared/build.gradle.kts
@@ -254,6 +254,17 @@ kotlin {
             }
         }
     }
+
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        if (this.konanTarget.family == org.jetbrains.kotlin.konan.target.Family.IOS) {
+            binaries {
+                framework {
+                    baseName = "app_shared"
+                    isStatic = true
+                }
+            }
+        }
+    }
 }
 
 dependencies {
