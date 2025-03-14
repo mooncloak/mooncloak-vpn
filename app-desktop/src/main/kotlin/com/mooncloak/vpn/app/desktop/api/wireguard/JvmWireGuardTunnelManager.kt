4749f897be248cbcb83c9f40b91a3a0d90c0ebb6
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-14 17:09:38 -0400

    Fixed JvmWireGuardTunnelManager + Upgraded Gradle to version 8.10

--- app-desktop/src/main/kotlin/com/mooncloak/vpn/app/desktop/api/wireguard/JvmWireGuardTunnelManager.kt
+++ app-desktop/src/main/kotlin/com/mooncloak/vpn/app/desktop/api/wireguard/JvmWireGuardTunnelManager.kt
@@ -4,35 +4,37 @@ import com.mooncloak.kodetools.konstruct.annotations.Inject
 import com.mooncloak.vpn.api.shared.server.Server
 import com.mooncloak.vpn.network.core.tunnel.Tunnel
 import com.mooncloak.vpn.network.core.tunnel.TunnelManager
+import com.mooncloak.vpn.util.shared.coroutine.ApplicationCoroutineScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
 internal class JvmWireGuardTunnelManager @Inject internal constructor(
-
+    private val coroutineScope: ApplicationCoroutineScope
 ) : TunnelManager {
 
     override val tunnels: StateFlow<List<Tunnel>>
-        get() = TODO("Not yet implemented")
+        get() = mutableTunnels.asStateFlow()
+
+    private val mutableTunnels = MutableStateFlow<List<Tunnel>>(emptyList())
 
     override suspend fun sync() {
-        TODO("Not yet implemented")
     }
 
-    override suspend fun connect(server: Server): Tunnel? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun connect(server: Server): Tunnel? = null
 
     override suspend fun disconnect(tunnelName: String) {
-        TODO("Not yet implemented")
     }
 
     override suspend fun disconnectAll() {
-        TODO("Not yet implemented")
     }
 
-    override fun subscribeToChanges(coroutineScope: CoroutineScope, poll: Duration): Job {
-        TODO("Not yet implemented")
-    }
+    override fun subscribeToChanges(coroutineScope: CoroutineScope, poll: Duration): Job =
+        coroutineScope.launch {
+            // TODO
+        }
 }
