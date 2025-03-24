9773c5344889fb53ba6200b5108dc76b6b2471b3
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-24 15:35:28 -0400

    Fixed building of sideloaded-direct Android app

--- app-android/src/direct/kotlin/com/mooncloak/vpn/app/android/di/AndroidDirectApplicationComponent.kt
+++ app-android/src/direct/kotlin/com/mooncloak/vpn/app/android/di/AndroidDirectApplicationComponent.kt
@@ -3,20 +3,25 @@ package com.mooncloak.vpn.app.android.di
 import com.mooncloak.kodetools.konstruct.annotations.Component
 import com.mooncloak.kodetools.konstruct.annotations.Provides
 import com.mooncloak.kodetools.konstruct.annotations.Singleton
-import com.mooncloak.kodetools.storagex.keyvalue.KeyValueStorage
-import com.mooncloak.kodetools.storagex.keyvalue.MutableKeyValueStorage
-import com.mooncloak.kodetools.storagex.keyvalue.Settings
-import com.mooncloak.vpn.app.android.AndroidAppClientInfo
+import com.mooncloak.vpn.api.shared.key.WireGuardConnectionKeyManager
+import com.mooncloak.vpn.app.android.api.wireguard.AndroidWireGuardConnectionKeyManager
+import com.mooncloak.vpn.app.android.api.wireguard.AndroidWireGuardTunnelManager
+import com.mooncloak.vpn.app.android.api.wireguard.WireGuardBackend
 import com.mooncloak.vpn.app.shared.di.ApplicationComponent
 import com.mooncloak.vpn.app.shared.info.AppClientInfo
 import com.mooncloak.vpn.app.shared.util.ApplicationContext
-import com.russhwolf.settings.Settings
-import kotlinx.serialization.json.Json
-import com.mooncloak.vpn.app.shared.api.network.AndroidLocalNetworkManager
-import com.mooncloak.vpn.app.shared.api.network.LocalNetworkManager
-import com.mooncloak.vpn.app.shared.api.vpn.TunnelManager
-import com.mooncloak.vpn.app.android.api.wireguard.WireGuardTunnelManager
-import com.mooncloak.vpn.app.shared.util.coroutine.ApplicationCoroutineScope
+import com.mooncloak.vpn.app.android.info.AndroidAppClientInfo
+import com.mooncloak.vpn.app.android.util.WireGuardVpnContextWrapper
+import com.mooncloak.vpn.data.sqlite.SqlDriverFactory
+import com.mooncloak.vpn.data.sqlite.database.MooncloakDatabase
+import com.mooncloak.vpn.data.sqlite.invoke
+import com.mooncloak.vpn.network.core.ip.LocalDeviceIPAddressProvider
+import com.mooncloak.vpn.network.core.ip.invoke
+import com.mooncloak.vpn.network.core.tunnel.TunnelManager
+import com.mooncloak.vpn.util.shared.coroutine.ApplicationCoroutineScope
+import com.mooncloak.vpn.util.shortcuts.AppShortcutManager
+import com.mooncloak.vpn.util.shortcuts.invoke
+import com.wireguard.android.backend.GoBackend
 
 @Component
 @Singleton
@@ -32,11 +37,13 @@ internal abstract class AndroidDirectApplicationComponent internal constructor(
 
     @Provides
     @Singleton
-    internal fun provideDatabaseDriverFactory(factory: AndroidDatabaseDriverFactory): DatabaseDriverFactory = factory
+    internal fun provideDatabaseDriverFactory(context: ApplicationContext): SqlDriverFactory =
+        SqlDriverFactory(context = context, schema = MooncloakDatabase.Schema)
 
     @Provides
     @Singleton
-    internal fun provideLocalNetworkManager(manager: AndroidLocalNetworkManager): LocalNetworkManager = manager
+    internal fun provideLocalNetworkManager(context: ApplicationContext): LocalDeviceIPAddressProvider =
+        LocalDeviceIPAddressProvider(context = context)
 
     @Provides
     @Singleton
@@ -45,11 +52,17 @@ internal abstract class AndroidDirectApplicationComponent internal constructor(
 
     @Provides
     @Singleton
-    internal fun provideWireGuardBackend(context: ApplicationContext): WireGuardBackend = GoBackend(context)
+    internal fun provideWireGuardBackend(context: ApplicationContext): WireGuardBackend =
+        GoBackend(WireGuardVpnContextWrapper(context))
 
     @Provides
     @Singleton
-    internal fun provideTunnelManager(manager: WireGuardTunnelManager): TunnelManager = manager
+    internal fun provideTunnelManager(manager: AndroidWireGuardTunnelManager): TunnelManager = manager
+
+    @Provides
+    @Singleton
+    internal fun provideAppShortcutManager(context: ApplicationContext): AppShortcutManager =
+        AppShortcutManager(context)
 }
 
 internal fun ApplicationComponent.Companion.create(
--- app-android/src/main/kotlin/com.mooncloak.vpn.app.android/di/AndroidPresentationComponent.kt
+++ app-android/src/main/kotlin/com.mooncloak.vpn.app.android/di/AndroidPresentationComponent.kt
@@ -1,50 +1,24 @@
 package com.mooncloak.vpn.app.android.di
 
 import android.app.Activity
-import androidx.activity.ComponentActivity
-import androidx.compose.ui.platform.UriHandler
-import com.mooncloak.kodetools.konstruct.annotations.Component
 import com.mooncloak.kodetools.konstruct.annotations.Provides
-import com.mooncloak.vpn.app.android.play.GooglePlayBillingManager
 import com.mooncloak.vpn.app.android.api.server.AndroidVPNConnectionManager
-import com.mooncloak.vpn.app.android.play.ServicePlansGooglePlaySource
-import com.mooncloak.vpn.api.shared.billing.BillingManager
-import com.mooncloak.vpn.api.shared.plan.ServicePlansRepository
 import com.mooncloak.vpn.network.core.tunnel.TunnelManager
 import com.mooncloak.vpn.network.core.tunnel.TunnelManagerPreparer
 import com.mooncloak.vpn.network.core.tunnel.invoke
 import com.mooncloak.vpn.network.core.vpn.VPNConnectionManager
 import com.mooncloak.vpn.app.android.util.AndroidAppShortcutProvider
-import com.mooncloak.vpn.app.shared.di.ApplicationComponent
 import com.mooncloak.vpn.app.shared.di.PresentationComponent
 import com.mooncloak.vpn.app.shared.di.PresentationScoped
 import com.mooncloak.vpn.app.shared.util.AndroidLibsLoader
 import com.mooncloak.vpn.app.shared.feature.dependency.util.LibsLoader
-import com.mooncloak.vpn.app.shared.util.ActivityContext
 import com.mooncloak.vpn.app.shared.util.AndroidSystemAuthenticationProvider
 import com.mooncloak.vpn.app.shared.util.SystemAuthenticationProvider
 import com.mooncloak.vpn.util.permission.PermissionHandler
 import com.mooncloak.vpn.util.permission.invoke
-import com.mooncloak.vpn.util.shared.coroutine.PresentationCoroutineScope
 import com.mooncloak.vpn.util.shortcuts.AppShortcutProvider
 
-@Component
-@PresentationScoped
-internal abstract class AndroidPresentationComponent internal constructor(
-    @Component internal val applicationComponent: ApplicationComponent,
-    @get:Provides override val presentationCoroutineScope: PresentationCoroutineScope,
-    @get:Provides override val uriHandler: UriHandler,
-    @get:Provides override val activityContext: ActivityContext,
-    @get:Provides override val activity: ComponentActivity
-) : PresentationComponent() {
-
-    @Provides
-    @PresentationScoped
-    internal fun provideBillingManager(manager: GooglePlayBillingManager): BillingManager = manager
-
-    @Provides
-    @PresentationScoped
-    internal fun providePlansRepository(source: ServicePlansGooglePlaySource): ServicePlansRepository = source
+internal abstract class AndroidPresentationComponent internal constructor() : PresentationComponent() {
 
     @Provides
     @PresentationScoped
@@ -80,16 +54,3 @@ internal abstract class AndroidPresentationComponent internal constructor(
     @PresentationScoped
     internal fun provideActivity(): Activity = activity
 }
-
-internal fun PresentationComponent.Companion.create(
-    applicationComponent: ApplicationComponent,
-    coroutineScope: PresentationCoroutineScope,
-    activity: ComponentActivity,
-    uriHandler: UriHandler
-): AndroidPresentationComponent = AndroidPresentationComponent::class.create(
-    applicationComponent = applicationComponent,
-    presentationCoroutineScope = coroutineScope,
-    activityContext = activity,
-    activity = activity,
-    uriHandler = uriHandler
-)
