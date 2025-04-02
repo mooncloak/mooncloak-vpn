c0b55d6060e4e6577ba9b917d71b8bd69d4b4dc8
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-02 12:27:19 -0400

    Created BaseCryptoWalletApi

---
+++ crypto-lunaris/src/commonMain/kotlin/com/mooncloak/vpn/crypto/lunaris/BaseCryptoWalletApi.kt
@@ -0,0 +1,17 @@
+package com.mooncloak.vpn.crypto.lunaris
+
+import com.mooncloak.vpn.crypto.lunaris.model.CryptoWallet
+import com.mooncloak.vpn.crypto.lunaris.provider.CryptoWalletAddressProvider
+import com.mooncloak.vpn.crypto.lunaris.repository.CryptoWalletRepository
+import com.mooncloak.vpn.crypto.lunaris.repository.getByAddressOrNull
+
+internal abstract class BaseCryptoWalletApi internal constructor(
+    private val cryptoWalletAddressProvider: CryptoWalletAddressProvider,
+    private val cryptoWalletRepository: CryptoWalletRepository
+) : CryptoWalletApi {
+
+    protected suspend fun getCurrentWalletOrNull(): CryptoWallet? =
+        cryptoWalletAddressProvider.get()?.let { address ->
+            cryptoWalletRepository.getByAddressOrNull(address)
+        }
+}
--- crypto-lunaris/src/commonMain/kotlin/com/mooncloak/vpn/crypto/lunaris/provider/CryptoWalletAddressProvider.kt
+++ crypto-lunaris/src/commonMain/kotlin/com/mooncloak/vpn/crypto/lunaris/provider/CryptoWalletAddressProvider.kt
@@ -6,7 +6,7 @@ import com.mooncloak.vpn.data.shared.provider.Provider
  * Provides the address of the current Crypto Wallet instance. This allows the user to select their current wallet and
  * maintain multiple wallets.
  */
-public fun interface CryptoWalletAddressProvider : Provider<String> {
+public fun interface CryptoWalletAddressProvider : Provider<String?> {
 
     public companion object
 }
