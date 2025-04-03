0f3bfe9c50675396d73afdbc56bf395cc1c268cb
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-03 11:39:52 -0400

    Added PromoDetails for Lunaris

--- app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/crypto/wallet/CryptoWalletViewModel.kt
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/crypto/wallet/CryptoWalletViewModel.kt
@@ -1,13 +1,19 @@
 package com.mooncloak.vpn.app.shared.feature.crypto.wallet
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CardGiftcard
 import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import com.mooncloak.kodetools.konstruct.annotations.Inject
 import com.mooncloak.kodetools.logpile.core.LogPile
 import com.mooncloak.kodetools.logpile.core.error
 import com.mooncloak.kodetools.statex.ViewModel
 import com.mooncloak.vpn.api.shared.VpnServiceApi
+import com.mooncloak.vpn.app.shared.feature.crypto.wallet.model.PromoDetails
 import com.mooncloak.vpn.app.shared.model.NotificationStateModel
 import com.mooncloak.vpn.app.shared.resource.Res
+import com.mooncloak.vpn.app.shared.resource.crypto_wallet_promo_description
+import com.mooncloak.vpn.app.shared.resource.crypto_wallet_promo_title
 import com.mooncloak.vpn.app.shared.resource.crypto_wallet_value_blockchain_ethereum
 import com.mooncloak.vpn.app.shared.resource.crypto_wallet_value_network_polygon
 import com.mooncloak.vpn.app.shared.resource.global_unexpected_error
@@ -16,12 +22,18 @@ import com.mooncloak.vpn.crypto.lunaris.model.CryptoWallet
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Month
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
 import org.jetbrains.compose.resources.getString
 
 @Stable
 public class CryptoWalletViewModel @Inject public constructor(
     private val cryptoWalletManager: CryptoWalletManager,
-    private val vpnServiceApi: VpnServiceApi
+    private val vpnServiceApi: VpnServiceApi,
+    private val clock: Clock
 ) : ViewModel<CryptoWalletStateModel>(initialStateValue = CryptoWalletStateModel()) {
 
     private val mutex = Mutex(locked = false)
@@ -32,6 +44,7 @@ public class CryptoWalletViewModel @Inject public constructor(
                 var blockChain: String? = null
                 var network: String? = null
                 var wallet: CryptoWallet? = null
+                var promoDetails: PromoDetails? = null
 
                 try {
                     emit { current -> current.copy(isLoading = true) }
@@ -39,13 +52,15 @@ public class CryptoWalletViewModel @Inject public constructor(
                     blockChain = getString(Res.string.crypto_wallet_value_blockchain_ethereum)
                     network = getString(Res.string.crypto_wallet_value_network_polygon)
                     wallet = cryptoWalletManager.getDefaultWallet()
+                    promoDetails = getPromoDetails(wallet = wallet)
 
                     emit { current ->
                         current.copy(
                             isLoading = false,
                             blockChain = blockChain,
                             network = network,
-                            wallet = wallet
+                            wallet = wallet,
+                            promo = promoDetails
                         )
                     }
                 } catch (e: Exception) {
@@ -60,6 +75,7 @@ public class CryptoWalletViewModel @Inject public constructor(
                             blockChain = blockChain,
                             network = network,
                             wallet = wallet,
+                            promo = promoDetails,
                             error = NotificationStateModel(
                                 message = getString(Res.string.global_unexpected_error)
                             )
@@ -72,5 +88,24 @@ public class CryptoWalletViewModel @Inject public constructor(
 
     public fun refresh() {
         // TODO
+    }
+
+    private suspend fun getPromoDetails(wallet: CryptoWallet?): PromoDetails? {
+        val now = clock.now()
+
+        // Arbitrary cut-off of May 5th, 2025
+        val expiration = LocalDate(year = 2025, month = Month.MAY, dayOfMonth = 5)
+            .atStartOfDayIn(timeZone = TimeZone.currentSystemDefault())
+
+        return when {
+            wallet != null -> null // TODO: Check if lunaris was gifted.
+            now < expiration -> PromoDetails(
+                title = getString(Res.string.crypto_wallet_promo_title),
+                description = getString(Res.string.crypto_wallet_promo_description),
+                icon = { rememberVectorPainter(Icons.Default.CardGiftcard) }
+            )
+
+            else -> null
+        }
     }
 }
