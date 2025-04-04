b1b851c47baf57dc06ed15863da80bef3baf416c
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-04 15:10:32 -0400

    Created RestoreWalletUseCase

---
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/feature/crypto/wallet/usecase/RestoreWalletUseCase.kt
@@ -0,0 +1,19 @@
+package com.mooncloak.vpn.app.shared.feature.crypto.wallet.usecase
+
+import com.mooncloak.kodetools.konstruct.annotations.Inject
+import com.mooncloak.vpn.crypto.lunaris.CryptoPasswordManager
+import com.mooncloak.vpn.crypto.lunaris.CryptoWalletManager
+import com.mooncloak.vpn.crypto.lunaris.getCurrentOrGenerate
+import com.mooncloak.vpn.crypto.lunaris.model.CryptoWallet
+
+public class RestoreWalletUseCase @Inject public constructor(
+    private val cryptoWalletManager: CryptoWalletManager,
+    private val cryptoPasswordManager: CryptoPasswordManager
+) {
+
+    public suspend operator fun invoke(seedPhrase: String): CryptoWallet {
+        val password = cryptoPasswordManager.getCurrentOrGenerate()
+
+        return cryptoWalletManager.restoreWallet(seedPhrase = seedPhrase, password = password)
+    }
+}
