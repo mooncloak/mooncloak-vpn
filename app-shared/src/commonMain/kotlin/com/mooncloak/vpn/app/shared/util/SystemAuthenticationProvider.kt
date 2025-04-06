79fc00231bceed8c1c3f03368015d019e82ef608
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-06 10:37:09 -0400

    Created SystemAuthenticationResult and created suspending variant of SystemAuthenticationProvider.launchAuthentication function

--- app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/util/SystemAuthenticationProvider.kt
+++ app-shared/src/commonMain/kotlin/com/mooncloak/vpn/app/shared/util/SystemAuthenticationProvider.kt
@@ -1,5 +1,8 @@
 package com.mooncloak.vpn.app.shared.util
 
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
 /**
  * A component that handles requiring system authentication. For instance, displaying a system biometric authentication
  * dialog to open the application. The credentials provided to the system are NOT accessible to the application. Not
@@ -43,4 +46,54 @@ public interface SystemAuthenticationProvider {
     )
 
     public companion object
+}
+
+/**
+ * Launches the system authentication prompt.
+ *
+ * @param [title] The title text to display in the system authentication prompt.
+ *
+ * @param [subtitle] The subtitle text to display in the system authentication prompt.
+ *
+ * @param [description] The description text to display in the system authentication prompt.
+ *
+ * @return [SystemAuthenticationResult]
+ */
+public suspend fun SystemAuthenticationProvider.launchAuthentication(
+    title: String,
+    subtitle: String? = null,
+    description: String? = null
+): SystemAuthenticationResult =
+    suspendCancellableCoroutine { continuation ->
+        this.launchAuthentication(
+            title = title,
+            subtitle = subtitle,
+            description = description,
+            onSuccess = { continuation.resume(SystemAuthenticationResult.Success) },
+            onFailed = { continuation.resume(SystemAuthenticationResult.Failure) },
+            onError = { code, message ->
+                continuation.resume(
+                    SystemAuthenticationResult.Error(
+                        code = code,
+                        message = message
+                    )
+                )
+            }
+        )
+    }
+
+/**
+ * Represents the result of launching an authentication request with [SystemAuthenticationProvider].
+ */
+public sealed interface SystemAuthenticationResult {
+
+    public data object Success : SystemAuthenticationResult
+
+    public data object Failure : SystemAuthenticationResult
+
+    public class Error public constructor(
+        public val code: Int? = null,
+        public val message: String? = null,
+        public val cause: Throwable? = null
+    ) : SystemAuthenticationResult
 }
