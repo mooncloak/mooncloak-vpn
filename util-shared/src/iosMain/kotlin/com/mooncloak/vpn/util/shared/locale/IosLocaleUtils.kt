13d575cc55c10bd5b1c63a588a1ee37f341c99a9
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-09 15:40:08 -0400

    Implemented IosLocaleUtils

--- util-shared/src/iosMain/kotlin/com/mooncloak/vpn/util/shared/locale/IosLocaleUtils.kt
+++ util-shared/src/iosMain/kotlin/com/mooncloak/vpn/util/shared/locale/IosLocaleUtils.kt
@@ -2,11 +2,33 @@ package com.mooncloak.vpn.util.shared.locale
 
 import com.mooncloak.kodetools.locale.ExperimentalLocaleApi
 import com.mooncloak.kodetools.locale.Locale
+import platform.Foundation.NSLocale
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.currentLocale
+import platform.Foundation.localeWithLocaleIdentifier
 
 @ExperimentalLocaleApi
 public actual val Locale.decimalSeparator: String
-    get() = TODO()
+    get() {
+        val nsLocale = NSLocale.localeWithLocaleIdentifier(languageTag.value)
+            ?: NSLocale.currentLocale()
+
+        val formatter = NSNumberFormatter().apply {
+            setLocale(nsLocale)
+        }
+
+        return formatter.decimalSeparator ?: "."
+    }
 
 @ExperimentalLocaleApi
 public actual val Locale.groupingSeparator: String
-    get() = TODO()
+    get() {
+        val nsLocale = NSLocale.localeWithLocaleIdentifier(languageTag.value)
+            ?: NSLocale.currentLocale()
+
+        val formatter = NSNumberFormatter().apply {
+            setLocale(nsLocale)
+        }
+
+        return formatter.groupingSeparator ?: ","
+    }
