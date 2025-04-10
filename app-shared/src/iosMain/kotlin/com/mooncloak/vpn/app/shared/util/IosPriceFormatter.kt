6b191bc0fc539cc3c22356c52fc261154a963733
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-10 16:59:19 -0400

    Implemented Price.format for iOS

--- app-shared/src/iosMain/kotlin/com/mooncloak/vpn/app/shared/util/IosPriceFormatter.kt
+++ app-shared/src/iosMain/kotlin/com/mooncloak/vpn/app/shared/util/IosPriceFormatter.kt
@@ -1,5 +1,25 @@
 package com.mooncloak.vpn.app.shared.util
 
 import com.mooncloak.vpn.api.shared.plan.Price
+import platform.Foundation.NSNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumberFormatterCurrencyStyle
+import kotlin.math.pow
 
-public actual fun Price.format(): String? = TODO()
+public actual fun Price.format(): String? {
+    // Create a NumberFormatter
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterCurrencyStyle
+        setCurrencyCode(currency.code.value)
+    }
+
+    // Convert amount from minor units to major units
+    val fractionDigits = currency.defaultFractionDigits ?: 2
+    if (fractionDigits < 0) return null
+
+    val divisor = 10.0.pow(fractionDigits.toDouble())
+    val decimalAmount = amount.toDouble() / divisor
+
+    // Format the number
+    return formatter.stringFromNumber(NSNumber(decimalAmount))
+}
