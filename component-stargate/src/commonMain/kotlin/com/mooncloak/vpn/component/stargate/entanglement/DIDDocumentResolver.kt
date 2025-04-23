3f03f3503518eebf6964d1b51786a90762d5be09
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-23 10:52:38 -0400

    Misc stargate updates

--- component-stargate/src/commonMain/kotlin/com/mooncloak/vpn/component/stargate/entanglement/DIDDocumentResolver.kt
+++ component-stargate/src/commonMain/kotlin/com/mooncloak/vpn/component/stargate/entanglement/DIDDocumentResolver.kt
@@ -4,19 +4,18 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-public class DIDDocumentResolver public constructor(
+public interface DIDDocumentResolver {
+
+    public suspend fun resolve(did: DID): DIDDocument
+
+    public companion object
+}
+
+public class DIDWebDocumentResolver public constructor(
     private val httpClient: HttpClient
-) {
+) : DIDDocumentResolver {
 
-    public suspend fun resolve(did: DID): DIDDocument =
-        when (val method = did.method) {
-            "web" -> resolveDidWeb(did)
-            "plc" -> resolveDidPlc(did)
-            // TODO: Possibly support other well known DID methods.
-            else -> error("Unsupported DID method '$method'")
-        }
-
-    private suspend fun resolveDidWeb(did: DID): DIDDocument {
+    override suspend fun resolve(did: DID): DIDDocument {
         val identifier = did.id ?: error("DID id part was required but was missing.")
 
         // Convert identifier to URL (e.g., chris.keenan -> https://chris.keenan/.well-known/did.json)
@@ -40,7 +39,18 @@ public class DIDDocumentResolver public constructor(
         }
     }
 
-    private suspend fun resolveDidPlc(did: DID): DIDDocument {
+    private fun DIDDocument.validateDidDocument(did: DID) {
+        if (this.id != did.value) {
+            error("DID document id '${this.id}' does not match expected DID '${did.value}'.")
+        }
+    }
+}
+
+public class DIDPlcDocumentResolver public constructor(
+    private val httpClient: HttpClient
+) : DIDDocumentResolver {
+
+    override suspend fun resolve(did: DID): DIDDocument {
         // Query PLC directory (e.g., https://plc.directory/did:plc:abc123)
         val url = "https://plc.directory/${did.value}"
 
@@ -56,4 +66,20 @@ public class DIDDocumentResolver public constructor(
             error("DID document id '${this.id}' does not match expected DID '${did.value}'.")
         }
     }
+}
+
+public class DefaultDIDDocumentResolver public constructor(
+    httpClient: HttpClient
+) : DIDDocumentResolver {
+
+    private val didWebDocumentResolver = DIDWebDocumentResolver(httpClient = httpClient)
+    private val didPlcDocumentResolver = DIDPlcDocumentResolver(httpClient = httpClient)
+
+    override suspend fun resolve(did: DID): DIDDocument =
+        when (val method = did.method) {
+            "web" -> didWebDocumentResolver.resolve(did)
+            "plc" -> didPlcDocumentResolver.resolve(did)
+            // TODO: Possibly support other well known DID methods.
+            else -> error("Unsupported DID method '$method'")
+        }
 }
--- component-stargate/src/commonMain/kotlin/com/mooncloak/vpn/component/stargate/entanglement/DIDResolver.kt
+++ component-stargate/src/commonMain/kotlin/com/mooncloak/vpn/component/stargate/entanglement/DIDResolver.kt
@@ -5,14 +5,27 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
-public class DIDResolver public constructor(
+public interface DIDResolver {
+
+    public suspend fun resolve(handle: IdentityHandle): DID
+
+    public companion object
+}
+
+public class ATProtocolDIDResolver public constructor(
     private val defaultDomainProvider: DomainProvider,
     private val dnsTxtRecordResolver: DnsTxtRecordResolver,
-    private val httpClient: HttpClient
-) {
+    private val httpClient: HttpClient,
+    private val json: Json
+) : DIDResolver {
 
-    public suspend fun resolve(handle: IdentityHandle): DID =
+    override suspend fun resolve(handle: IdentityHandle): DID = coroutineScope {
         when (handle.format) {
             IdentityHandleFormat.DID -> DID.from(value = handle.value)
 
@@ -38,29 +51,67 @@ public class DIDResolver public constructor(
                 // contain different separators (ex: '@' for ActivityPub or ':' for Matrix). We are flexible for what
                 // we accept for a user handle but we have to format it properly for resolution and we currently only
                 // support the AT Protocol for DID resolution.
-                val value = "$localPart.$remotePart"
+                val handleValue = "$localPart.$remotePart"
 
                 // See the AT Protocol documentation for resolving the DID from the user handle:
                 // https://atproto.com/specs/handle
 
-                val record = dnsTxtRecordResolver.resolve("_atproto.$value")
-                    .firstOrNull { it.startsWith("did=") }
-                    ?.substringAfter("did=")
-                    ?.let { DID.fromOrNull(value = it) }
+                // From the documentation:
+                //
+                // > It is ok to attempt both resolution methods in parallel, and to use the first successful result
+                // > available. If the two methods return conflicting results (aka, different DIDs), the DNS TXT result
+                // > should be preferred, though it is also acceptable to record the result as ambiguous and try again
+                // > later.
+                val deferredDns = async {
+                    dnsTxtRecordResolver.resolve("_atproto.$handleValue")
+                        .firstOrNull { it.startsWith("did=") }
+                        ?.substringAfter("did=")
+                        ?.let { DID.fromOrNull(value = it) }
+                }
 
-                if (record != null) {
-                    record
-                } else {
-                    val httpResponse = httpClient.get("https://$value/.well-known/atproto-did")
+                val deferredWellKnown = async {
+                    val httpResponse = httpClient.get("https://$handleValue/.well-known/atproto-did")
 
                     if (!httpResponse.status.isSuccess()) {
-                        error("Failed to resolve DID.")
+                        null
+                    } else {
+                        DID.fromOrNull(value = httpResponse.bodyAsText())
                     }
-
-                    DID.from(value = httpResponse.bodyAsText())
                 }
+
+                // If we can't resolve the other values, try and guess the PDS server from the handle and attempt to
+                // resolve it that way.
+                val deferredPds = async {
+                    val httpResponse =
+                        httpClient.get("https://$remotePart/xrpc/com.atproto.identity.resolveHandle?handle=$handleValue")
+
+                    if (!httpResponse.status.isSuccess()) {
+                        null
+                    } else {
+                        try {
+                            val jsonText = httpResponse.bodyAsText()
+                            val did = json.decodeFromString(deserializer = PdsDid.serializer(), string = jsonText)
+
+                            DID.fromOrNull(value = did.did)
+                        } catch (_: Exception) {
+                            null
+                        }
+                    }
+                }
+
+                val dnsDid = deferredDns.await()
+                val wellKnownDid = deferredWellKnown.await()
+                val pdsDid = deferredPds.await()
+
+                dnsDid ?: wellKnownDid ?: pdsDid ?: error("Failed to resolve DID.")
             }
         }
+    }
 
     public companion object
 }
+
+@Serializable
+internal data class PdsDid internal constructor(
+    @SerialName(value = "did") val did: String
+)
